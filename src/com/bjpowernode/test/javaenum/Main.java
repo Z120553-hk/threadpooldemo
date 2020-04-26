f0659b2cbf9aba0900c20e53926686e05dee01de
hk <1433174408@qq.com>
2020-04-27 07:29:13 +0800

    hk-branch

--- src/com/bjpowernode/test/javaenum/Main.java
+++ src/com/bjpowernode/test/javaenum/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("adjust-branch");
         System.out.println("test-branch");
+        System.out.println("hk-branch");
     }
 }
